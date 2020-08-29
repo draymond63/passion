63f9cc7f3fc5ff72b41df76b3db43b10db5ee727
Daniel Raymond <dan@raymond.ch>
2020-08-29 19:55:49 -0400

    Removed w2v

--- init.py
+++ init.py
@@ -7,14 +7,8 @@ from tfidf import tfidf_cluster
 from tfidf.tfidf_vectorizer import tfidf_vecs
 from path.career_path_compile import compile_prereq_graph
 
-from w2v.w2v_train_compile import compile_w2v_data
-from w2v.w2v_career_map import create_w2v_map, display_map
-from w2v import w2v_cluster
-
 CREATE_DATA = True
 CREATE_PATH_GRAPH = True
-CREATE_W2V_MAP = True
-DISPLAY_W2V_MAP = False
 
 if __name__ == "__main__":
     # * Clean the data and assign key tags to each job (based off term frequency)
@@ -29,10 +23,6 @@ if __name__ == "__main__":
         compile_prereq_graph()
 
     # * Creates an N-D map of the careers based on who had what jobs
-    if CREATE_W2V_MAP:
-        compile_w2v_data()
-        create_w2v_map()            # space_dim = 25
-        w2v_cluster.append_keys()
-    if DISPLAY_W2V_MAP:
-        display_map()
+              
+
 
