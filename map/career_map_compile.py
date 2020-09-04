3a07eec02fcb8876b5145255e80873c7c7e40f33
Daniel Raymond <dan@raymond.ch>
2020-09-04 09:12:41 -0400

    Restructured tfidf and general cluster

---
+++ map/career_map_compile.py
@@ -0,0 +1,111 @@
+import pandas as pd
+import numpy as np
+import plotly.express as px
+from sklearn.manifold import TSNE
+from sklearn.decomposition import PCA
+
+from Passion.general import cluster
+
+# (Positive) Pointwise mutual information --> log(p(a,b) / ( p(a) * p(b) ))
+# * Standardizes the data in the matrix so that prevalence of a job doesn't affect things
+def pmi_matrix(df: pd.DataFrame, positive=False):
+    # Convert matrix into probablities
+    col_totals = df.sum(axis=0)
+    total = col_totals.sum()
+    row_totals = df.sum(axis=1)
+    expected = np.outer(row_totals, col_totals) / total
+    df = df / expected
+    # Silence distracting warnings about log(0):
+    with np.errstate(divide='ignore'):
+        df = np.log(df)
+    df[np.isinf(df)] = 0.0  # log(0) = 0
+    # Set minimum to 0's
+    if positive:
+        df[df < 0] = 0.0
+    return df
+
+def create_career_map(og_file='dump_cleaned.csv', new_file='career_map.csv', title_col='tfidfKey', new_col='cmapKey', space_dim=None, do_cluster=True):
+    df = pd.read_csv(og_file)
+
+    # * Creating a huge matrix where each row is a job and each column is a coordinate
+    # Initially each coordinate is the number of people that had both of those jobs
+    job_array = df[title_col].unique()
+    job_keys = {key:i for i, key in enumerate(job_array)}
+    # Creat the initial 2D Matrix
+    init_dim = len(job_keys)
+    co_occerrence = np.zeros((init_dim, init_dim))
+
+    # Iterate through each person in the group
+    people = df.groupby('memberUrn')
+    for _, person in people:
+        # Make sure they at least have two different jobs
+        if person[title_col].nunique() > 1:
+            # Make sure each job is in the cmap
+            for job1 in person[title_col]:
+                for job2 in person[title_col]:
+                    coords = (job_keys[job1], job_keys[job2])
+                    co_occerrence[coords] += 1
+
+    # Reorganize data
+    co_occerrence = pd.DataFrame(co_occerrence)
+    job_array = pd.Series(job_array, name=title_col)
+
+    cmap = pmi_matrix(co_occerrence)
+    # Join the title keys
+    cmap = cmap.join(job_array)
+    print(cmap.head())
+    print(cmap.shape)
+    # Remove useless rows and columns
+    # Row dropping only focuses on columns named 0..~540 (everything but title_col)
+    cmap.dropna(how='all', axis=1, inplace=True)    
+    cmap.dropna(how='all', subset=[*range(len(cmap))], inplace=True) # ! THIS IS BROKEN
+    print(cmap.head())
+    print(cmap.shape)
+
+    if do_cluster:
+        categories = cluster(vecs=cmap.drop(title_col, axis=1), title_column=df[title_col], new_col=new_col, cluster_threshold=25)
+        cmap = pd.merge(cmap, categories, on=title_col)
+        # print(cmap.head())
+
+    # Collapse matrix if requested
+    if space_dim:
+        # PCA works for higher dimensions than TSNE
+        pca = PCA(n_components=space_dim, random_state=0)
+        collapsed = pca.fit_transform(cmap.drop(title_col, axis=1))
+        # Use the new data as the columns
+        collapsed = pd.DataFrame(collapsed)
+        cmap = pd.merge(cmap[title_col], collapsed)
+
+    print(f"\nPMI'd {'& Collapsed ' if space_dim else ''}Co-occurence Career Map")
+    print(cmap.shape)
+    print(cmap.head())
+
+    # Save the data if requested
+    if new_file:
+        cmap.to_csv(new_file)
+    return cmap
+
+def display_map(cmap, groupNumCol='cmapKey', groupNameCol='tfidfKey'):
+    non_data_cols = [groupNumCol, groupNameCol]
+    data = cmap.drop(non_data_cols, axis=1)
+
+    # Reducing dimensions
+    if len(cmap.columns) != 2:
+        tsne = TSNE(n_components=2, random_state=0, verbose=1)
+        vecs = tsne.fit_transform(data)
+        data = pd.DataFrame(vecs)
+
+    cmap = data.join(cmap.filter(non_data_cols))
+    print(cmap.head())
+
+    fig = px.scatter(cmap, 
+        x=0, y=1,
+        color=groupNumCol,
+        hover_name=groupNameCol
+    )
+    fig.show()
+
+if __name__ == "__main__":
+    cmap = create_career_map()
+    print(cmap.shape)
+    # display_map(cmap)
--- tfidf/tfidf_cluster.py
+++ tfidf/tfidf_cluster.py
@@ -1,25 +1,92 @@
-from Passion.general import cluster
+from Passion.general import cluster, keys_to_str
 import pandas as pd
+
+# * Names the groups based off of the titles
+def name_group(grouped_freqs: pd.DataFrame, grouped_titles: pd.Series) -> str:
+    assert len(grouped_freqs) == len(grouped_titles), f'Frequencies and titles must have the same number of rows, freqs: {len(grouped_freqs)}, titles: {len(grouped_titles)}'
+
+    # Average together the tfidf columns to find the most common words
+    word_freqs = grouped_freqs.mean()
+    # Get the mose popular words from the group
+    keys = word_freqs.nlargest(2, keep='all')
+    # We actually want the indices since that is where the words are
+    keys = list(keys.index)
+
+    # Since we are using all, if a word has way too many keys, that means it was only meant to have 1
+    if len(keys) > 3: 
+        keys = word_freqs.nlargest(1, keep='all')
+        keys = list(keys.index)
+    # If the keys are still too long, it doesn't belong to any category
+    if len(keys) > 2:
+        keys = 'misc'
+    # Convert list to job title (string)
+    else:
+        keys = keys_to_str(keys, grouped_titles)
+    
+    # Append the job_keys
+    return keys
+
+def combine_misc(df, numCol: str, titleCol: str):
+    # Gather the indices of each misc group
+    misc_groups = []
+    for groupName, groupNum in zip(df[titleCol], df[numCol]):
+        if groupName == 'misc' and groupNum not in misc_groups:
+            misc_groups.append(groupNum)
+    
+    print(f'Merging {len(misc_groups)} miscellaneous groups into one')
+
+    # Merge all the misc groups into one
+    # Iterate through all useless misc groups, putting the largest groupNums in their place
+    for groupNum in misc_groups[1:]:
+        # Replace all the old misc category numbers with the main misc number
+        df[numCol] = df[numCol].apply(
+            lambda num: misc_groups[0] if num == groupNum else num)
+        # Move the largest categories number to the one that is now empty
+        df[numCol] = df[numCol].apply(
+            lambda num: groupNum if num == len(df)-1 else num)
+
+    return df
 
 # * Using clustering to append the cleaned dump with job key
 def append_keys(appended_file='dump_cleaned.csv', input_file='tfidf/tfidf_positions.csv', new_col='tfidfKey', cluster_threshold=1):
     print('\nDATA WITH TFIDF CLUSTERED GROUPINGS')
     df = pd.read_csv(input_file)
-    categories = cluster(df, new_col, cluster_threshold=cluster_threshold)
+    data = df.drop(['posTitle'], axis=1)
+    titles = df['posTitle']
 
-    # * Merging into the dump_cleaned
-    dump = pd.read_csv(appended_file)
-    # Only append the dump if the data isn't already there
-    if new_col not in dump:
-        # Merge the group numbers on the posTitles first
-        dump = pd.merge(dump, categories, on='posTitle')
-        # Sort the rows to bring it back to where it was before
-        dump = dump.sort_values(['memberUrn', 'startDate'], ascending=False)
-        # Wewrite the dump to include the group number and job keys
-        dump.to_csv(appended_file, index=False)
+    # Add groups to the data
+    new_col_num = new_col+'Num'
+    categories = cluster(data, titles, new_col_num, cluster_threshold=cluster_threshold)
+    new_df = pd.merge(df, categories, on='posTitle')
+    grouped_df = new_df.groupby(new_col_num)
     
+    # Name each group
+    job_keys = [0] * len(grouped_df)
+    for i, group in grouped_df:
+        # Gather the data for the cluster
+        vecs = group.drop(['posTitle', new_col_num], axis=1)
+        group_titles = group['posTitle']
+        # Save the name
+        job_keys[i] = name_group(vecs, group_titles)
+
+    job_keys = pd.Series(job_keys, name=new_col)
+    categories = categories.join(job_keys)
+
+    # Merging into the dump_cleaned
+    dump = pd.read_csv(appended_file)
+    # Merge the group numbers on the posTitles first
+    dump = pd.merge(dump, categories, on='posTitle')
+    # Sort the rows to bring it back to where it was before
+    dump = dump.sort_values(['memberUrn', 'startDate'], ascending=False)
+    # Combine misc groups together
+    dump = combine_misc(dump, new_col_num, new_col)
+
+    # Only append the dump if the data isn't already there
+    dump.to_csv(appended_file, index=False)
+
+    print(f'Final number of tfidf groups: {max(dump[new_col_num])}')
+    print(dump.shape)
     print(dump.head())
 
-
 if __name__ == "__main__":
-    append_jobkeys()
+    append_keys()
