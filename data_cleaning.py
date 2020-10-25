91dbfd4cf4b9e2a4e1ac3bf6fe1789716586922c
Daniel Raymond <dan@raymond.ch>
2020-10-25 14:06:14 -0400

    Reconfigure to wiki data

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,99 +1,17 @@
 import pandas as pd
-from nltk import word_tokenize, download
-from nltk.stem import WordNetLemmatizer
-download(['punkt', 'wordnet'])
+from general import DUMP, POP_DUMP, COLUMNS
 
-replacements = {
-    'cio': 'chief information officer', 
-    'ceo': 'chief information officer', 
-    'cfo': 'chief executive officer', 
-    'coo': 'chief financial officer', 
-    'cmo': 'chief marketing officer', 
-    'cto': 'chief technology officer', 
-    '-': '', 
-    'frontend': 'front end',
-    'bi': 'business intelligence', 
-    'dba': 'database administrator', 
-    'qa': 'quality assurance', 
-    'pmo': 'project management office', 
-    'pr': 'public relations', 
-    'hr': 'human resources', 
-    'sr.': 'senior', 
-    'sr': 'senior', 
-    'snr': 'senior', 
-    'vp': 'vice president', 
-    'adviser': 'advisor', 
-    'sap': 'system applications', 
-    'leader': 'manager', 
-    'lead': 'manager',
-    'gi specialist': 'gastroenterologist',
-    'associate': '',
-}
+def get_popularity():
+    print('Reading in data...')
+    df = pd.read_csv(DUMP, sep='\t', names=COLUMNS) # compression='gzip'
+    df = df.filter(['site', 'amt'], axis=1)
+    print('Data read')
 
-def lemmatize(item: str) -> str:
-    stop_words = ['ios', 'vice']
+    pop = df.groupby('site')['amt'].apply(sum)
+    print(pop.head())
+    print(pop.shape)
+    pop.to_csv(POP_DUMP, sep='\t')
 
-    wordnet_lemmatizer = WordNetLemmatizer()
-    # Break the string into a list of words
-    words = word_tokenize(item)
-    # Reduce each word to a lemma
-    for i, word in enumerate(words): 
-        if word not in stop_words:                   
-            new_word = wordnet_lemmatizer.lemmatize(word)
-            words[i] = new_word
-    # Replace the entry with the final item
-    new_item = ' '.join(words)
-    return new_item
-
-def clean_data(og_file='dump.csv', new_file='dump_cleaned.csv', rep_times=3):
-    ### Kaggle import: https://github.com/Kaggle/kaggle-api
-    # kaggle datasets download -f dump.csv --unzip killbot/linkedin-profiles-and-jobs-data
-    df = pd.read_csv(r'dump.csv')
-
-    # Filter for useful entries 
-    df = df.filter(items=['memberUrn', 'posTitle', 'startDate', 'endDate'])
-    # Simplify the member id
-    df['memberUrn'] = df['memberUrn'].apply(lambda x: int(x.split(':')[-1]))
-    # Lowercase every job
-    df['posTitle'] = df['posTitle'].str.lower()
-
-    # * Character removal 
-    # Remove the end of entries with the following symbols
-    df['posTitle'] = df['posTitle'].apply(lambda x: x.split('(')[0])
-    df['posTitle'] = df['posTitle'].apply(lambda x: x.split(',')[0])
-    df['posTitle'] = df['posTitle'].apply(lambda x: x.split('|')[0])
-    df['posTitle'] = df['posTitle'].apply(lambda x: x.split(':')[0])
-    df['posTitle'] = df['posTitle'].apply(lambda x: x.split('/')[0])
-    df['posTitle'] = df['posTitle'].apply(lambda x: x.split(' - ')[0])
-    df['posTitle'] = df['posTitle'].apply(lambda x: x.strip())
-
-    # * Substring Replacement
-    # Add regex operators before and after each key to make sure we aren't matching in another word
-    # https://stackoverflow.com/questions/6713310/regex-specify-space-or-start-of-string-and-space-or-end-of-string
-    reg_repl = {}
-    for key in replacements:
-        reg_repl[r'(^|\s)(' + key + r')[\b\s]'] = ' ' + replacements[key] + ' '
-    df['posTitle'].replace(regex=reg_repl, inplace=True)
-    # * Lemmatization
-    df['posTitle'] = df['posTitle'].apply(lemmatize)
-
-    # Remove columns with 'and' or '&' in them?
-
-    # * Final touches
-    # Remove entries that don't appear that often
-    df = df[df.groupby('posTitle')['posTitle'].transform('count') >= rep_times]
-    # Drop all duplicates and the NAs except for the end dates
-    df = df.dropna(subset=['memberUrn', 'posTitle', 'startDate'])
-    df = df.drop_duplicates()
-    # Sort values just for nicety
-    df = df.sort_values(['memberUrn', 'startDate'], ascending=False)
-    df = df.reset_index().drop('index', axis=1)
-    # Save data
-    df.to_csv('dump_cleaned.csv', index=False)
-    print('\nCLEANED DATA')
-    print(df.head())
-    print(df.shape)
-    print('Size:', df.memory_usage().sum() // 2**10, 'KiB')
 
 if __name__ == "__main__":
-    clean_data()
+    get_popularity()
--- general.py
+++ general.py
@@ -1,91 +1,14 @@
-### THIS CLUSTERS DATA AND APPENDS THEIR CLASSIFICATION TO THE dump_cleaned CSV 
-# Rescources:
-# https://scikit-learn.org/stable/modules/clustering.html
 
-import pandas as pd
-from sklearn.cluster import AgglomerativeClustering
-
-# * Default Clustering algorithm
-def hierachical_cluster(data, threshold=1):
-    cl = AgglomerativeClustering(
-        n_clusters=None,
-        distance_threshold=threshold,
-        linkage='ward',
-    )
-    return cl.fit_predict(data)
-
-# * Converts a list of key words into a searchable job
-def keys_to_str(keys: list, jobs: (list, pd.Series), use_full_title=True) -> str:
-    # Find a job that contains all the keys
-    for job in jobs:
-        good_job = True
-        
-        for key in keys:
-            if key not in job:
-                good_job = False
-        
-        # If the job contains all the keys, find the order and return them
-        if good_job:
-            # Use only the keys to make the string
-            if not use_full_title:
-                # Grab starting index of each word
-                placement = {key: job.find(key) for key in keys}
-                # Sort words into a list by their index
-                words = sorted(placement, key=placement.get)
-                # Join all the words with spaces
-                return ' '.join(words)
-            # Or use (almost) the full title
-            else:
-                # Find when the first word appears
-                beginning = min([job.find(key) for key in keys])
-                # Remove the unimportant intro words
-                return job[beginning:]
-
-    raise AssertionError(f'No job contained all the keys in the group {keys}, suggesting a bad grouping')
-
-def name_group(titles: pd.Series) -> str:
-    # Concatentate all the words
-    total = ' '.join(titles)
-    # Get all the unique words in the series
-    words = set(total.split(' '))
-    # Remove words that are fillers
-    stop_words = set(['in', 'of', '-', '&', 'the', 'a', 'an'])
-    words = words - stop_words
-
-    counts = {}
-    for word in words:
-        counts[word] = total.count(word)
-
-    return max(counts, key=counts.get)
+#                        ref                site      path  amt
+# 0   Bathtubs_Over_Broadway  Industrial_musical      link   97
+# 1              other-empty  Industrial_musical  external   88
+# 2         Industrial_music  Industrial_musical      link   67
+# Unique Sites: 4156721
+# Unique Refs:  2006915
+# Shape:  (30009674, 4)
+DUMP = 'storage/clickstream-enwiki-2020-09.tsv'
 
 
-# * Combines all groups in the list into one
-def combine_group(df, groups, col: str):
-    # Merge all the groups into one
-    # Iterate through all useless groups, putting the largest groupNums in their place
-    for group in groups[1:]:
-        # Replace all the old misc category numbers with the main misc number
-        df[col] = df[col].apply(
-            lambda x: groups[0] if x == group else x)
-        # Move the largest categories number to the one that is now empty
-        df[col] = df[col].apply(
-            lambda x: group if x == len(df)-1 else x)
-    return df
+POP_DUMP = 'storage/wiki-popularity.tsv'
 
-# * Matches the title column with a group numbering and a list of keys that relate to each group number
-def cluster(vecs: pd.DataFrame, new_col: str, title_column=None, cluster_threshold=1) -> pd.DataFrame:
-    # Result in is an np.ndarray of numerical categories
-    groups = hierachical_cluster(vecs, cluster_threshold)
-    groups = pd.Series(groups, name=new_col)
-
-    print('Clustering grouped', len(groups), 'jobs into', groups.nunique(), 'groups')
-    # Merge the title column so that it has something to merge with
-    if isinstance(title_column, pd.Series):
-        assert len(groups) == len(title_column), f'vecs must be the same length as the title column, not {len(vecs)}, {len(title_column)}'
-        # Reset the index to make sure concatentation works as intended
-        title_column.reset_index(inplace=True, drop=True)
-        groups = pd.concat([groups, title_column], axis=1)
-    else:
-        groups = pd.DataFrame(groups)
-
-    return groups
+COLUMNS = ['ref', 'site', 'path', 'amt']
