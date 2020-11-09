1bd4baf6ea8b920d516e284971408de8cd034f83
Daniel Raymond <dan@raymond.ch>
2020-11-09 11:23:40 -0500

    Calculate user position in the map

--- use/wiki_suggest.py
+++ use/wiki_suggest.py
@@ -15,43 +15,32 @@ class UserSuggester():
         self.centers['l3'] = df.groupby('l3').agg(lambda x: sum(x)/len(x))
         # Get beginning user coordinate
         self.likes = self.translate(words)
-        self.position = self.get_coord() # Uses self.likes
-        self.radius = self.get_radius() # ? Encompass all liked wikis
-        self.learning_rate = 10 # ? How to set an initial value 
+        points = self.get_like_points()
+        self.position = points.sum() / len(points) # Average like points
+        self.radius = max(np.sum((points - self.position)**2, axis=1)) # Encompass all liked wikis
+        self.learning_rate = self.radius * 2 # ? How to set an initial value ?
 
+    # Convert words to wikis
     def translate(self, words):
-        translations = {}
+        # Index by name so we can use the filter function (and the 'like' paramater)
+        name_categories = self.categories.reset_index('site')
+        name_categories = name_categories.set_index('name')
+        # Get the site corresponding to each word
+        translations = []
         for word in words:
-            for key in self.categories:                    
-                if word in self.categories[key]:
-                    translations[word] = key
-                
-            # ! else find similar words !
-        return pd.DataFrame(translations, columns=['key'])
+            sites = name_categories.filter(like=word, axis='index')
+            translations.extend(sites['site'])
+        return translations
 
     def get_like_points(self):
-        print(self.likes)
-
-        site_likes = self.likes[self.likes['key'] == 'site']
-        points = self.map.filter(site_likes.index)
-
-        for key in ('l1', 'l2', 'l3'):
-            key_likes = self.likes[self.likes['key'] == key]
-            key_points = self.centers[key].filter(key_likes.index)
-            points.append(key_points)
-        return points
-
-    def get_coord(self):
-        points = self.get_like_points()
-        print(points.head(), points.shape)
-        return points.sum() / len(points)
-
-    # Distance from self.position to furtherest point in self.likes
-    def get_radius(self):
-        points = self.get_like_points()
-        dist_2 = np.sum((points - self.position)**2, axis=1) # Calculate distance from all point
-        print(dist_2)
-        return min(dist_2)
+        return self.map.filter(self.likes, axis='index')
+    
+    # Recommend 3 things
+    # - within the radius that isn't liked yet (to change positon/learning-rate)
+    # - outside the radius                     (to change radius)
+    # - random                                 (to change position)
+    def recommend(self):
+        pass
             
 
     
@@ -61,8 +50,8 @@ class UserSuggester():
 # Take in user profile and suggest
 
 if __name__ == "__main__":
-    us = UserSuggester(['People', 'Lebron James'])
-    # print(us.recommend())
+    us = UserSuggester(['LeBron James', 'Basketball', 'National Basketball Association'])
+    print(us.recommend())
 
 # * SURVEY:
 # Tell us what you currently like
